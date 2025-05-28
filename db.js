3bd40fd479911c3108853ab18b0f09c6335e91d0
solace06 <khushi.sharma061101@gmail.com>
2025-05-28 12:46:24 +0530

    connecting to the database and added dev script

---
+++ db.js
@@ -0,0 +1,12 @@
+// db.js
+//connecting to the database
+const { Sequelize } = require('sequelize');
+const path = require('path');
+
+const db = new Sequelize({
+  dialect: 'sqlite',
+  storage: path.resolve(__dirname, 'database.sqlite'),
+  logging: false       
+});
+
+module.exports = db;
